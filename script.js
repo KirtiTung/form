ea84a5d24f3e2677530b98ca2678b08c08bd357f
bishal9861 <bishalpatel9861@gmail.com>
2022-01-23 19:44:16 +0530

    ADDED SOME JS CODE
    GET AUTH DETAILS

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const labels=document.querySelectorAll('.form-control label')
+const inpForm=document.getElementById("authForm")
+const submitBtn=document.getElementById("submitBtn")
 
 labels.forEach(label=>{
     label.innerHTML=label.innerText
     .split('')
     .map((letter,idx)=>`<span style="transition-delay:${idx*50}ms">${letter}</span>`)
     .join('')
+})
+//function to get auth details
+const getAuthDetails=()=>{
+    console.log(document.getElementById("userName").value)
+    console.log(document.getElementById("userPassword").value)
+    return [document.getElementById("userName").value,document.getElementById("userPassword").value]
+}
+
+submitBtn.addEventListener("click",()=>{
+    let authDetails=getAuthDetails();
+    
 })
